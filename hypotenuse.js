b581c0726601db0c5bd1d33a281bb6f42893c620
Bilal-9 <bilalhussain1115@gmail.com>
2022-05-19 11:25:58 -0400

    updates

---
+++ hypotenuse.js
@@ -0,0 +1,30 @@
+const sides = document.querySelectorAll(".side-input");
+const BtnHypo = document.querySelector("#hypotenuse-btn");
+const outputEL = document.querySelector("#Output");
+
+function calculatesumofsquares(a, b) {
+    const sumofsqures = a * a + b * b;
+    // console.log(sumofsqures);
+    return (sumofsqures);
+
+}
+
+// function calculatehypotenuse() {
+//     const sumofsqures = calculatesumofsquares(sides[0].value, sides[1].value);
+//     const lengthofhypotenuse = Math.sqrt(sumofsqures);
+//     //     console.log(sumofsqures);
+
+function calculatehypotenuse() {
+    const sumofsqures = calculatesumofsquares(Number(sides[0].value), Number(sides[1].value));
+    const lengthofhypotenuse = Math.sqrt(sumofsqures);
+
+    // console.log(lengthofhypotenuse);
+    outputEL.innerText = " The Length of hypotenuse is " + lengthofhypotenuse;
+
+
+
+
+
+}
+
+BtnHypo.addEventListener("click", calculatehypotenuse);
